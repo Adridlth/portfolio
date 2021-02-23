774dad1a0694bbd159db356af3151b7f593269dd
Adridlth <35839603+Adridlth@users.noreply.github.com>
2021-02-23 17:29:12 +0100

    Create year.js

---
+++ js/year.js
@@ -0,0 +1,11 @@
+/*Automatic year
+*/
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "UA-109062924-1");
+    </script>
